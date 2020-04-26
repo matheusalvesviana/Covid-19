4df9ee0f2a385f05b09fdaf9d6c3953a72224a17
Matheus Alves Viana <theus_a7x@hotmail.com>
2020-04-26 18:42:40 -0300

    Add runCycle method implementation

--- app/src/main/java/com/example/covid_19/MainActivity.kt
+++ app/src/main/java/com/example/covid_19/MainActivity.kt
@@ -4,15 +4,17 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.covid_19.objects.People
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    private var infectedPeople: Int = 0
-    private var recoveredPeople: Int = 0
-    private var healthyPeople: Int = 0
-    private val matrix = Array(10) { IntArray(10) }
+    private var infectedPeople: MutableList<People> = mutableListOf()
+    private var recoveredPeople: MutableList<People> = mutableListOf()
+    private var healthyPeople: MutableList<People> = mutableListOf()
+    private var matrix = Array(10) { IntArray(10) }
     private var counter = 0
     private var peopleInfected = false
     private var randomValue = Random().nextInt(3)
@@ -44,13 +46,18 @@ class MainActivity : AppCompatActivity() {
     private fun createMatrix() {
 
         while (counter < matrix.size) {
+            val people: People
             val randRow = Random().nextInt(matrix.size)
             val randColumn = Random().nextInt(matrix.size)
             if (matrix[randRow][randColumn] == 0) {
                 if (!peopleInfected && randomValue == 2) {
                     matrix[randRow][randColumn] = 2
+                    people = People(randRow, randColumn, true)
+                    infectedPeople.add(people)
                     peopleInfected = true
                 } else if (randomValue == 1) {
+                    people = People(randRow, randColumn, false)
+                    healthyPeople.add(people)
                     matrix[randRow][randColumn] = 1
                 }
                 randomValue = if (peopleInfected) {
@@ -64,8 +71,12 @@ class MainActivity : AppCompatActivity() {
         refreshMatrix()
         pb_circular.visibility = View.GONE
     }
+    private fun applyPeoplePosition(peoples: List<People>){
+        peoples.map { matrix[it.row][it.column] = if (it.isInfected) 2 else 1 }
+    }
 
-    private fun refreshMatrix(){
+    private fun refreshMatrix() {
+        tv_matrix.text = ""
         for (i in matrix.indices) {
             for (element in matrix[i]) {
                 if (element != 0) tv_matrix.text = tv_matrix.text.toString() + " $element "
@@ -77,6 +88,26 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun runCycle(cycles: Int) {
+        matrix = Array(10) { IntArray(10) }
+        Toast.makeText(
+            this,
+            "Pessoas saudaveis: ${healthyPeople.size} \nPessoas infectadas: ${infectedPeople.size}",
+            Toast.LENGTH_LONG
+        ).show()
+        for (i in 0 until cycles) {
+            infectedPeople.map {
+                it.column = Random().nextInt(10)
+                it.row = Random().nextInt(10)
+            }
+
+            healthyPeople.map {
+                it.column = Random().nextInt(10)
+                it.row = Random().nextInt(10)
+            }
+            applyPeoplePosition(infectedPeople)
+            applyPeoplePosition(healthyPeople)
+            refreshMatrix()
+        }
 
     }
 }
