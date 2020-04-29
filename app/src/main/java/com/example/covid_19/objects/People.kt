764d651817b21bad6ff6d4c5cf2a6a4f9f646999
Matheus Alves Viana <theus_a7x@hotmail.com>
2020-04-28 21:20:24 -0300

    Add random boolean to isGroupOfRisk variable

--- app/src/main/java/com/example/covid_19/objects/People.kt
+++ app/src/main/java/com/example/covid_19/objects/People.kt
@@ -1,8 +1,11 @@
 package com.example.covid_19.objects
 
+import kotlin.random.Random
+
 data class People (
     var row:Int,
     var column :Int,
     var isInfected: Boolean,
-    var infectedTime: Int
+    var infectedTime: Int,
+    var isGroupOfRisk: Boolean = Random.nextBoolean()
 )
